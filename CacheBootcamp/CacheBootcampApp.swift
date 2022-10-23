1e53c510ca4de70c34ab625ec2ffe31e325d30a1
hazem-smawy <hazemsmawy@gmail.com>
2022-10-23 09:48:36 +0300

    Initial Commit

---
+++ CacheBootcamp/CacheBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheBootcampApp.swift
+//  CacheBootcamp
+//
+//  Created by hazem smawy on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CacheBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
